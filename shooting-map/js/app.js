4b5021056d2bcfb787b8e443cb4925575e74a229
Mitchell Amundsen <mitchell.k.amundsen@gmail.com>
2015-10-19 19:26:51 -0700

    transfer to desktop

--- shooting-map/js/app.js
+++ shooting-map/js/app.js
@@ -2,3 +2,11 @@
 
 'use strict';
 
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.mapbox.com/v4/mapbox.streets/51.505/-0.09/13.png?access_token=pk.eyJ1IjoibWl0Y2hlbGxhbXVuZHNlbiIsImEiOiJjaWZ5YXZ2ejY1MDNmdHRtMXMxN3Rya3QyIn0.jirD-JAi2WCl4wft_KTKUA'), {
+	attribution:
+	maxZoom: 18,
+	id: 'mitchellamundsen.cifyavupf6cf1oekrtrcbopck',
+	accessToken: 'pk.eyJ1IjoibWl0Y2hlbGxhbXVuZHNlbiIsImEiOiJjaWZ5YXZ2ejY1MDNmdHRtMXMxN3Rya3QyIn0.jirD-JAi2WCl4wft_KTKUA'
+}
