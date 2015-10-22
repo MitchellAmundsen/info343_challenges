3a1a0c882f85ff2eee0bd9c9ba2dad806a43684e
Mitchell Amundsen <mitchell.k.amundsen@gmail.com>
2015-10-21 17:26:37 -0700

    almost done

--- shooting-map/js/app.js
+++ shooting-map/js/app.js
@@ -2,7 +2,6 @@
 
 'use strict';
 
-
 	var map = L.map('map-container').setView([45.475, -100.348], 4);
 
 	L.tileLayer('https://api.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWl0Y2hlbGxhbXVuZHNlbiIsImEiOiJjaWZ5YXZ2ejY1MDNmdHRtMXMxN3Rya3QyIn0.jirD-JAi2WCl4wft_KTKUA', {
@@ -13,23 +12,25 @@
 			}).addTo(map);
 
 
-	var armedGroup = L.layerGroup();
-	var unarmedGroup = L.layerGroup();
-
-	var armedArray = [];
-	var unarmedArray = [];
+	
 
 	var dataProcess = function(data){
+		var armedGroup = L.layerGroup();
+		var unarmedGroup = L.layerGroup();
+
+		var armedArray = [];
+		var unarmedArray = [];
+
 		for(var i=0; i<data.length; i++){
 			var current = data[i];
 			var armed = current.armed;
-			
+
 			if(armed==true){
 				var marker = L.circle([current.lat, current.lng], 1000, {
 					color: 'red',
 					fillColor: '#f03',
 					fillOpacity: 0.5
-				});
+				}).bindPopup(current.summary);
 				marker.addTo(armedGroup);
 				armedArray.push(current);
 			} 
@@ -38,12 +39,11 @@
 					color: 'blue',
 					fillColor: '#1E90FF',
 					fillOpacity: 0.5
-				});
+				}).bindPopup(current.summary);
 				marker.addTo(unarmedGroup);
 				unarmedArray.push(current);
 			}
 		}
-		
 		var overlay = {
 			"Armed" : armedGroup,
 			"Unarmed" : unarmedGroup
@@ -54,16 +54,32 @@
 
 		L.control.layers(null, overlay, options).addTo(map);
 
-		if(options)
-		// how do you call checkbox for groups (armed unarmed)
-		//popup not working
+		var shotsCount = function(array){
+			var totalShots = 0;
+			for(var i=0; i<array.length; i++){
+				totalShots += array[i].victim.age;
+			}
+			return totalShots;
+		}
+		var ageCount = function(array){
+			var totalAge = 0;
+			for(var i=0; i<array.length; i++){
+				totalAge = array[i].shots;
+			}
+			return totalAge;
+		}
 
+		var shotsArmed = shotsCount(armedArray);
+		var shotsUnarmed = shotsCount(unarmedArray);
 
+		var ageArmed = ageCount(armedArray);
+		var ageUnarmed = ageCount(unarmedArray);
+
+		map.on(layerAdd, function(){
+			
+		})
 
 	};
 
 	$.getJSON('data/data.min.json').then(dataProcess);
-
-
-
 
