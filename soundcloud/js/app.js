f22e870045bf234a48797f79c9ea595ccebfc09a
Mitchell Amundsen <mitchell.k.amundsen@gmail.com>
2015-11-03 22:01:21 -0800

    finished

--- soundcloud/js/app.js
+++ soundcloud/js/app.js
@@ -6,6 +6,13 @@ var CLIENT_ID = 'd2de86b6f2a8c564b00e1f78421fab9d' //application ID for requests
 var myApp = angular.module('myApp', [])
   .controller('MyCtrl', ['$scope', '$http', function($scope, $http) { 
   
+  	$scope.currentSong = '';
+  	$scope.playSong = SC.oEmbed('http://soundcloud.com/forss/flickermood', {
+  			auto_play: false
+		}).then(function(embed){
+  			console.log('oEmbed response: ', embed);
+		});
+
     //function called to fetch tracks based on the scope's query
     $scope.getTracks = function() {
       var request = BASE_URL + '/tracks' + '?' +'client_id='+ CLIENT_ID + '&q=' + $scope.query; //build the RESTful request
@@ -14,4 +21,5 @@ var myApp = angular.module('myApp', [])
           $scope.tracks = response.data; //save results to available model
         });
     };
+
 }])
